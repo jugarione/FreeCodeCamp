e97931cf019f572b15b0ab77ec8ca8eaa9809597
jugarione <jugarione@users.noreply.github.com>
2020-10-06 06:35:35 -0300

    firt Commit.

---
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/BasicAlgorithmsScripting.js
@@ -0,0 +1,97 @@
+/* Hello, this is all the exercices I made in FCC (FreeCodeCamp) Roadmap
+   I make them all by myself with research in google and redoing courses in FCC
+   so i can say that's all my work, and not a copyfrom others.
+*/
+
+// Convert Celsius to Fahrenheit
+
+
+// Passed
+
+
+// Reverse a String
+
+
+// Passed
+
+
+// Factorialize a Number
+
+
+// Passed
+
+
+// Find the Longest Word in a String
+
+
+// Passed
+
+
+// Return Largest Numbers in Arrays
+
+
+// Passed
+
+
+// Confirm the Ending
+
+
+// Passed
+
+
+// Repeat a String Repeat a String
+
+
+// Passed
+
+
+// Truncate a String
+
+
+// Passed
+
+
+// Finders Keepers
+
+
+// Passed
+
+
+// Boo who
+
+
+// Passed
+
+
+// Title Case a Sentence
+
+
+// Passed
+
+
+// Slice and Splice
+
+
+// Passed
+
+
+// Falsy Bouncer
+
+
+// Passed
+
+
+// Where do I Belong
+
+
+// Passed
+
+
+// Mutations
+
+
+// Passed
+
+
+// Chunky Monkey
+
---
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -0,0 +1,133 @@
+/* Hello, this is all the exercices I made in FCC (FreeCodeCamp) Roadmap
+   I make them all by myself with research in google and redoing courses in FCC
+   so i can say that's all my work, and not a copyfrom others.
+*/
+
+// Introduction to the Intermediate Algorithm Scripting Challenges
+
+
+// Passed
+
+
+// Sum All Numbers in a Range
+
+
+// Passed
+
+
+// Diff Two Arrays
+
+
+// Passed
+
+
+// Seek and Destroy
+
+
+// Passed
+
+
+// Wherefore art thou
+
+
+// Not Passed
+
+
+// Spinal Tap Case
+
+
+// Not Passed
+
+
+// Pig Latin
+
+
+// Not Passed
+
+
+// Search and Replace
+
+
+// Not Passed
+
+
+// DNA Pairing
+
+
+// Not Passed
+
+
+// Missing letters
+
+
+// Not Passed
+
+
+// Sorted Union
+
+
+// Not Passed
+
+
+// Convert HTML Entities
+
+
+// Not Passed
+
+
+// Sum All Odd Fibonacci Numbers
+
+
+// Not Passed
+
+
+// Sum All Primes
+
+
+// Not Passed
+
+
+// Smallest Common Multiple
+
+
+// Not Passed
+
+
+// Drop it
+
+
+// Not Passed
+
+
+// Steamroller
+
+
+// Not Passed
+
+
+// Binary Agents
+
+
+// Not Passed
+
+
+// Everything Be True
+
+
+// Not Passed
+
+
+// Arguments Optional
+
+
+// Not Passed
+
+
+// Make a Person
+
+
+// Not Passed
+
+
+// Map the Debris
+
