622dd679b14eea4870c9b435a67b8b1c5bc59542
jugarione <jugarione@users.noreply.github.com>
2020-10-07 16:19:41 -0300

    Added Spinal Tap Case, and Pig Latin

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -6,127 +6,153 @@
 // Introduction to the Intermediate Algorithm Scripting Challenges
 
 
-// Passed
-
 
 // Sum All Numbers in a Range
 
-
-// Passed
 
 
 // Diff Two Arrays
 
 
-// Passed
-
 
 // Seek and Destroy
-
-
-// Passed
 
 
 // Wherefore art thou
 
 
-// Not Passed
-
-
 // Spinal Tap Case
 
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+function spinalCase(str) {
 
-// Not Passed
+   let replaced = str.replace(/([a-z])([A-Z])/g, '$1 $2')
+   let split = replaced.split(/\W|_/);
 
+   console.log(split);
+   let result = split.join("-");
+   let lowerCase = result.toLowerCase();
+   console.log(lowerCase)
+   return lowerCase;
+}
+/* The results i have to make it work
+spinalCase("This Is Spinal Tap")
+spinalCase("thisIsSpinalTap")
+spinalCase("The_Andy_Griffith_Show")
+spinalCase("Teletubbies say Eh-oh")
+spinalCase("AllThe-small Things")
+*/
 
 // Pig Latin
 
+/*
+Pig Latin is a way of altering English Words. The rules are as follows:
 
-// Not Passed
+- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add "ay" to it.
+
+- If a word begins with a vowel, just add "way" at the end.
+
+Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+*/
+
+function translatePigLatin(str) {
+
+   let arr = str.split("");
+   let consonantRegex = /[bcdfghjklmnpqrstvwxys]+/i;
+   let vowelRegex = /[aeiou]/gi;
+   let result;
+
+   function allStartConsonant(array) {
+      let consonantCluster = [];
+      for (let i = 0; i < array.length; i++) {
+         if (consonantRegex.test(arr[0]) == true) {
+
+            consonantCluster.push(arr.shift());
+         } else {
+            return consonantCluster.join("");
+         }
+      }
+      return consonantCluster.join("");
+   }
+
+
+   if (str.search(vowelRegex) == -1) {
+      arr.push("ay")
+      result = arr.join("");
+      return result;
+   }
+
+
+   if (consonantRegex.test(arr[0]) == true) {
+      let first = allStartConsonant(arr);
+      arr.push(first + "ay")
+      result = arr.join("");
+   } else {
+      arr.push("way");
+      result = arr.join("")
+   }
+   return result;
+}
+// console.log(translatePigLatin("consonant"));
+/*
+   translatePigLatin("consonant");
+*/
 
 
 // Search and Replace
 
 
-// Not Passed
-
 
 // DNA Pairing
 
-
-// Not Passed
 
 
 // Missing letters
 
 
-// Not Passed
-
 
 // Sorted Union
 
-
-// Not Passed
 
 
 // Convert HTML Entities
 
 
-// Not Passed
-
 
 // Sum All Odd Fibonacci Numbers
 
-
-// Not Passed
 
 
 // Sum All Primes
 
 
-// Not Passed
-
 
 // Smallest Common Multiple
 
-
-// Not Passed
 
 
 // Drop it
 
 
-// Not Passed
-
 
 // Steamroller
 
-
-// Not Passed
 
 
 // Binary Agents
 
 
-// Not Passed
-
 
 // Everything Be True
 
-
-// Not Passed
 
 
 // Arguments Optional
 
 
-// Not Passed
-
 
 // Make a Person
 
-
-// Not Passed
 
 
 // Map the Debris
