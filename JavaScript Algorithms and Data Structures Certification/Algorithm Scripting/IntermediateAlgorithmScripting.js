24db77989b45f6624cbcdbd48184694ece0378c9
jugarione <jugarione@users.noreply.github.com>
2020-10-09 11:06:34 -0300

    Added DNA Pairing

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -146,6 +146,52 @@ myReplace("Let us get back to more Coding", "Coding", "algorithms") should retur
 
 
 // DNA Pairing
+/*
+The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+*/
+
+function pairElement(str) {
+
+   let dispairElements = str.split("");
+   let result = [];
+
+   for (let i = 0; i < dispairElements.length; i++) {
+      switch (dispairElements[i]) {
+         case "G":
+            result.push(["G", "C"]);
+            break;
+         case "C":
+            result.push(["C", "G"]);
+            break;
+         case "T":
+            result.push(["T", "A"]);
+            break;
+         case "A":
+            result.push(["A", "T"]);
+            break;
+      }
+   }
+
+   return result;
+}
+
+pairElement("GCG");
+
+/*
+Expected Results
+
+pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+pairElement("TTGAG") should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+pairElement("CTCTA") should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
+*/
 
 
 
