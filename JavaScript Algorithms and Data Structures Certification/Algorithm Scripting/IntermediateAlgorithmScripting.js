5908df199c202d27b9f6351c958b3d988cf4c797
jugarione <jugarione@users.noreply.github.com>
2020-10-09 16:37:53 -0300

    Added Sorted Union

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -217,8 +217,33 @@ fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined.
 */
 
 // Sorted Union
+/*
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+Check the assertion tests for examples.
+*/
 
 
+function uniteUnique(...arr) {
+   let allTogether = [];
+   let result = [];
+
+   allTogether = allTogether.concat(...arr);
+
+   for (let i = 0; i < allTogether.length; i++) {
+      if (result.lastIndexOf(allTogether[i]) == -1) {
+         result.push(allTogether[i]);
+      }
+   }
+   return result;
+}
+
+/*
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
+*/
 
 // Convert HTML Entities
 
