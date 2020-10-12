388f3fc7283447a3814aa61105038af71f6efa48
jugarione <jugarione@users.noreply.github.com>
2020-10-12 16:45:28 -0300

    Added Sum All Primes

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -315,7 +315,7 @@ function sumFibs(num) {
    }
 
    let onlyOdd = fib.filter(int => int % 2);
-   let result = onlyOdd.reduce((asd, int) => asd + int, 0);
+   let result = onlyOdd.reduce((back, int) => back + int, 0);
 
    console.log(fib)
    return result;
@@ -332,8 +332,36 @@ sumFibs(75025) should return 135721.
 
 
 // Sum All Primes
+/*
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+*/
 
+function sumPrimes(num) {
 
+   let numbersList = [];
+
+   for (let i = 2; i <= num; i++) {
+      numbersList.push(i);
+   }
+
+   let onlyPrimes = numbersList.filter(int => {
+      for (let i = 2; i < int; i++) {
+         if (int % i === 0) return false;
+      }
+      return true;
+   })
+   let result = onlyPrimes.reduce((back, post) =>
+      back + post)
+
+   return result;
+}
+
+/*
+sumPrimes(10) should return a number.
+sumPrimes(10) should return 17.
+sumPrimes(977) should return 73156.
+*/
 
 // Smallest Common Multiple
 
