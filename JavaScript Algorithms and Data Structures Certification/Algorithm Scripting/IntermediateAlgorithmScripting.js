d4896b13c08ed745a968777b6e8b8a3852dd9e72
jugarione <jugarione@users.noreply.github.com>
2020-10-12 18:00:52 -0300

    Added Smallest Common Multiple

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -364,7 +364,53 @@ sumPrimes(977) should return 73156.
 */
 
 // Smallest Common Multiple
+/*
+Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+The range will be an array of two numbers that will not necessarily be in numerical order.
+For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
+*/
 
+function smallestCommons(arr) {
+
+   let numArr = [];
+   let result = [];
+   // numArr have all the numbers between arr[0] and arr[2]
+   function arrNum() {
+      if (arr[0] < arr[1]) {
+         for (let i = arr[0]; i <= arr[1]; i++) {
+            numArr.push(i);
+         }
+      } else {
+         for (let i = arr[1]; i <= arr[0]; i++) {
+            numArr.push(i);
+         }
+      }
+   }
+   arrNum()
+
+   //
+
+   for (let mul = 1; mul <= 99999999; mul++) {
+      result = [];
+      for (let i = 0; mul % numArr[i] === 0; i++) {
+         result.push(true)
+         if (result.length == numArr.length) {
+            console.log(mul);
+            return mul;
+         }
+      }
+   }
+   return arrNum;
+}
+
+/*
+smallestCommons([1, 5]) should return a number.
+smallestCommons([1, 5]) should return 60.
+smallestCommons([5, 1]) should return 60.
+smallestCommons([2, 10]) should return 2520.
+smallestCommons([1, 13]) should return 360360.
+smallestCommons([23, 18]) should return 6056820.
+*/
 
 
 // Drop it
