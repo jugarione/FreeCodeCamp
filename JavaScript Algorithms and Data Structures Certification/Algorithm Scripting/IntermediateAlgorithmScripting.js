9d63efeaf055d7267b4f09260dc8ac389effe05e
jugarione <jugarione@users.noreply.github.com>
2020-10-12 11:36:10 -0300

    Added Convert HTML Entities

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -246,8 +246,51 @@ uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 
 */
 
 // Convert HTML Entities
+/*
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+*/
 
+function convertHTML(str) {
+   let regex = new RegExp(/&|<|>|"|'/g)
 
+   let match = str.match(regex)
+   let result = str;
+
+   if (match !== null) {
+      for (let i = 0; i < match.length; i++) {
+         switch (match[i]) {
+            case '&':
+               result = str.replace("&", "&amp;");
+               break;
+            case '<':
+               result = result.replace("<", "&lt;");
+               break;
+            case '>':
+               result = result.replace(">", "&gt;");
+               break;
+            case '"':
+               result = result.replace('"', "&quot;");
+               break;
+            case "'":
+               result = result.replace("'", "&apos;");
+               break;
+         }
+      }
+   }
+
+   console.log(result);
+   return result;
+}
+
+/*
+convertHTML("Dolce & Gabbana") should return "Dolce &amp; Gabbana".
+convertHTML("Hamburgers < Pizza < Tacos") should return "Hamburgers &lt; Pizza &lt; Tacos".
+convertHTML("Sixty > twelve") should return "Sixty &gt; twelve".
+convertHTML('Stuff in "quotation marks"') should return "Stuff in &quot;quotation marks&quot;".
+convertHTML("Schindler's List") should return "Schindler&apos;s List".
+convertHTML("<>") should return "&lt;&gt;".
+convertHTML("abc") should return "abc".
+*/
 
 // Sum All Odd Fibonacci Numbers
 
