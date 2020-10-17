e5569f24b69f98ed3cd998971d952bb14a2e9783
jugarione <jugarione@users.noreply.github.com>
2020-10-17 13:35:59 -0300

    Added binaryAgent

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -462,8 +462,36 @@ Your solution should not use the Array.prototype.flat() or Array.prototype.flatM
 */
 
 // Binary Agents
+/*
+Return an English translated sentence of the passed binary string.
+The binary string will be space separated.
+*/
 
+function binaryAgent(str) {
 
+   let alphabet = "abcdefghijklmnopqrstuvwxyz"
+   let simbols = " !\"#$%&'()*+,-./0123456789:;<=>?@"
+   let toProcess = str.split(" ")
+   let result = [];
+
+   toProcess.map(item => {
+      let letter = 0;
+      (item[7] == 1 ? letter = letter + 1 : null);
+      (item[6] == 1 ? letter = letter + 2 : null);
+      (item[5] == 1 ? letter = letter + 4 : null);
+      (item[4] == 1 ? letter = letter + 8 : null);
+      (item[3] == 1 ? letter = letter + 16 : null);
+      (item[1] == 0 ? result.push(simbols[letter]) : (item[2] == 1 ? result.push(alphabet[letter - 1]) : result.push(alphabet[letter - 1].toUpperCase())));
+   });
+   return result.join("");
+}
+
+/*
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111")
+should return "Aren't bonfires fun!?"
+binaryAgent("01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001")
+should return "I love FreeCodeCamp!"
+*/
 
 // Everything Be True
 
