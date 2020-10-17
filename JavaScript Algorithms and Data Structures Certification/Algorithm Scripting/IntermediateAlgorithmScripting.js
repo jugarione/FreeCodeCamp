aa554f102163ad8ef6b2091a6449213b730233bc
jugarione <jugarione@users.noreply.github.com>
2020-10-17 13:33:52 -0300

    Added Steamroller

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -444,8 +444,22 @@ dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) should return [3, 9, 
 
 // Steamroller
 
+/*
+Flatten a nested array. You must account for varying levels of nesting.
+*/
 
+function steamrollArray(arr) {
+   let flat = [].concat(...arr);
+   return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+}
 
+/*
+steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
+steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].
+steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4].
+steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4].
+Your solution should not use the Array.prototype.flat() or Array.prototype.flatMap() methods.
+*/
 
 // Binary Agents
 
