d5dee25f756879e84f5559075570ba5349c5e871
jugarione <jugarione@users.noreply.github.com>
2020-10-18 12:24:52 -0300

    Added addTogether and comment binaryAgent

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -475,7 +475,7 @@ function binaryAgent(str) {
    let simbols = " !\"#$%&'()*+,-./0123456789:;<=>?@"
    let toProcess = str.split(" ")
    let result = [];
-
+   // toProcess make the BinarySum and push the letter in the alphabet with the index
    toProcess.map(item => {
       let letter = 0;
       (item[7] == 1 ? letter = letter + 1 : null);
@@ -524,8 +524,40 @@ truthCheck([{"single": "double"}, {"single": NaN}], "single") should return fals
 */
 
 // Arguments Optional
+/*
+Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+Calling this returned function with a single argument will then return the sum:
+var sumTwoAnd = addTogether(2);
+sumTwoAnd(3) returns 5.
+If either argument isn't a valid number, return undefined.
+*/
 
+function addTogether(a, b) {
+   if (typeof a != "number") { return undefined };
 
+   if (typeof b == "number") {
+      return a + b;
+   } else if (typeof b == "undefined") {
+      return (n => {
+         if (typeof n === "number") {
+            return a + n;
+         } else {
+            return undefined;
+         }
+      });
+   }
+   return undefined;
+}
+
+/*
+addTogether(2, 3) should return 5.
+addTogether(23, 30) should return 53.
+addTogether(5)(7) should return 12.
+addTogether("http://bit.ly/IqT6zt") should return undefined.
+addTogether(2, "3") should return undefined.
+addTogether(2)([3]) should return undefined.
+*/
 
 // Make a Person
 
