d6edb7c82e9ef83a2a9e3263f4db016737c0c4cf
jugarione <jugarione@users.noreply.github.com>
2020-10-20 09:43:02 -0300

    Added Make a Person

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -560,8 +560,79 @@ addTogether(2)([3]) should return undefined.
 */
 
 // Make a Person
+//link from FCC https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/make-a-person
+/*
+Fill in the object constructor with the following methods below:
 
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.
+*/
 
+var Person = function (firstAndLast) {
+   let fullName = firstAndLast;
+   let splitName = fullName.split(" ")
+   let firstName = splitName[0];
+   let lastName = splitName[1];;
+   this.getFirstName = function () {
+      return firstName;
+   }
+   this.getLastName = function () {
+      return lastName;
+   }
+   this.getFullName = function () {
+      return fullName;
+   }
+   this.setFirstName = function (first) {
+      firstName = first;
+      splitName.shift();
+      splitName.unshift(firstName);
+      fullName = splitName.join(" ");
+   }
+   this.setLastName = function (last) {
+      lastName = last;
+      splitName.pop();
+      splitName.push(lastName);
+      fullName = splitName.join(" ");
+   }
+   this.setFullName = function (firstAndLast) {
+      fullName = firstAndLast
+      splitName = fullName.split(" ")
+      firstName = splitName[0];
+      lastName = splitName[1];;
+   }
+}
+var bob = new Person('Bob Ross');
+bob.getFullName();
+
+/*
+Object.keys(bob).length should return 6.
+bob instanceof Person should return true.
+bob.firstName should return undefined.
+bob.lastName should return undefined.
+bob.getFirstName() should return "Bob".
+bob.getLastName() should return "Ross".
+bob.getFullName() should return "Bob Ross".
+bob.getFullName() should return "Haskell Ross" after bob.setFirstName("Haskell").
+bob.getFullName() should return "Haskell Curry" after bob.setLastName("Curry").
+bob.getFullName() should return "Haskell Curry" after bob.setFullName("Haskell Curry").
+bob.getFirstName() should return "Haskell" after bob.setFullName("Haskell Curry").
+bob.getLastName() should return "Curry" after bob.setFullName("Haskell Curry").
+*/
 
 // Map the Debris
+/*
+
+*/
+
+
+
+/*
+
+*/
+
 
