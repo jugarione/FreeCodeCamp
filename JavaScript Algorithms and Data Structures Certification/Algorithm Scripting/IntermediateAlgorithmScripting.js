4397400aaa12481dd0eca03585a69d0938b5baf3
jugarione <jugarione@users.noreply.github.com>
2020-10-21 17:22:17 -0300

    Added Sum All Numbers in a Rangue

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/IntermediateAlgorithmScripting.js
@@ -8,8 +8,29 @@
 
 
 // Sum All Numbers in a Range
+/*
+We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
+For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10
+*/
 
+function sumAll(arr) {
+   let min = Math.min(...arr);
+   let max = Math.max(...arr);
+   let result = 0;
 
+   for (let i = min; i <= max; i++) {
+      result = result + i
+   }
+   return result;
+}
+
+/*
+sumAll([1, 4]) should return a number.
+sumAll([1, 4]) should return 10.
+sumAll([4, 1]) should return 10.
+sumAll([5, 10]) should return 45.
+sumAll([10, 5]) should return 45.
+*/
 
 // Diff Two Arrays
 
