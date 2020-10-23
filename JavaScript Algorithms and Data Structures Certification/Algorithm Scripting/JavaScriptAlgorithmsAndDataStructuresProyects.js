8d71f02aa5b54f36e3204f59a0b8fa45e86b626c
jugarione <jugarione@users.noreply.github.com>
2020-10-23 13:15:02 -0300

    Added Roman Numeral Converter

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/JavaScriptAlgorithmsAndDataStructuresProyects.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/JavaScriptAlgorithmsAndDataStructuresProyects.js
@@ -56,14 +56,87 @@ palindrome("five|_/|four") should return false.
 
 //Roman Numeral Converter
 /*
-
+Convert the given number into a roman numeral.
+All roman numerals answers should be provided in upper-case.
 */
 
+function convertToRoman(num) {
+    let romanNumber = [];
+    let number = num
+    while (number > 0) {
+        if (number >= 1000) {
+            romanNumber.push("M");
+            number = number - 1000;
+        } else if (number >= 900) {
+            romanNumber.push("CM");
+            number = number - 900;
+        } else if (number >= 500) {
+            romanNumber.push("D");
+            number = number - 500;
+        } else if (number >= 400) {
+            romanNumber.push("CD");
+            number = number - 400;
+        } else if (number >= 100) {
+            romanNumber.push("C");
+            number = number - 100;
+        } else if (number >= 90) {
+            romanNumber.push("XC");
+            number = number - 90;
+        } else if (number >= 50) {
+            romanNumber.push("L");
+            number = number - 50;
+        } else if (number >= 40) {
+            romanNumber.push("XL");
+            number = number - 40;
+        } else if (number >= 10) {
+            romanNumber.push("X");
+            number = number - 10;
+        } else if (number >= 9) {
+            romanNumber.push("IX");
+            number = number - 9;
+        } else if (number >= 5) {
+            romanNumber.push("V");
+            number = number - 5;
+        } else if (number >= 4) {
+            romanNumber.push("IV");
+            number = number - 4;
+        } else if (number >= 1) {
+            romanNumber.push("I");
+            number = number - 1;
+        }
+    }
 
+    return romanNumber.join("");
+}
 
 /*
-
+convertToRoman(2) should return "II".
+convertToRoman(3) should return "III".
+convertToRoman(4) should return "IV".
+convertToRoman(5) should return "V".
+convertToRoman(9) should return "IX".
+convertToRoman(12) should return "XII".
+convertToRoman(16) should return "XVI".
+convertToRoman(44) should return "XLIV".
+convertToRoman(45) should return "XLV"
+convertToRoman(68) should return "LXVIII"
+convertToRoman(83) should return "LXXXIII"
+convertToRoman(97) should return "XCVII"
+convertToRoman(99) should return "XCIX"
+convertToRoman(400) should return "CD"
+convertToRoman(500) should return "D"
+convertToRoman(501) should return "DI"
+convertToRoman(649) should return "DCXLIX"
+convertToRoman(798) should return "DCCXCVIII"
+convertToRoman(891) should return "DCCCXCI"
+convertToRoman(1000) should return "M"
+convertToRoman(1004) should return "MIV"
+convertToRoman(1006) should return "MVI"
+convertToRoman(1023) should return "MXXIII"
+convertToRoman(2014) should return "MMXIV"
+convertToRoman(3999) should return "MMMCMXCIX"
 */
+
 //Caesars Cipher
 /*
 
