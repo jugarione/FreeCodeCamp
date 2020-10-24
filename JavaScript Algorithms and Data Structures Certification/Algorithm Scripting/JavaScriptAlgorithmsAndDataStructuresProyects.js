07a351f12286a51bfe8d0ef748c89aa3d6c1a3b9
jugarione <jugarione@users.noreply.github.com>
2020-10-24 08:49:27 -0300

    Added Telephone Number Validator

--- JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/JavaScriptAlgorithmsAndDataStructuresProyects.js
+++ JavaScript Algorithms and Data Structures Certification/Algorithm Scripting/JavaScriptAlgorithmsAndDataStructuresProyects.js
@@ -157,7 +157,6 @@ function rot13(str) {
             result.push(str[i])
         }
     }
-    console.log(result)
     return result.join("");
 }
 
@@ -170,14 +169,75 @@ rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to THE QUICK
 
 //Telephone Number Validator
 /*
-
+Return true if the passed string looks like a valid US phone number.
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.i
 */
 
+function telephoneCheck(str) {
+    let result = true;
 
+    // first the cheat for this case telephoneCheck("(555)5(55?)-5555")
+    if (/\?/.test(str)) { return false; }
+
+    // test Open closed parenthesis and only 3 digits between parenthesis
+    if ((/\)/.test(str) || /\(/.test(str)) == false) {
+        result = true;
+    } else if ((/\)/.test(str) && /\(/.test(str)) == false) {
+        result = false;
+    } else if (/\(\d{3}\)/.test(str) == false) {
+        result = false;
+    }
+
+    // filter by amount of digits, 10 or 11 pass.
+    let test = str.match(/[0-9]/gi).length
+    if (test < 10) { return false; }
+    if (test > 11) {
+        return false;
+    }
+    // filter us country code == 1
+    if (test == 11 && str[0] != 1) {
+        return false;
+    }
+    return result;
+}
 
 /*
-
+telephoneCheck("555-555-5555") should return a boolean.
+telephoneCheck("1 555-555-5555") should return true.
+telephoneCheck("1 (555) 555-5555") should return true.
+telephoneCheck("5555555555") should return true.
+telephoneCheck("555-555-5555") should return true.
+telephoneCheck("(555)555-5555") should return true.
+telephoneCheck("1(555)555-5555") should return true.
+telephoneCheck("555-5555") should return false.
+telephoneCheck("5555555") should return false.
+telephoneCheck("1 555)555-5555") should return false.
+telephoneCheck("1 555 555 5555") should return true.
+telephoneCheck("1 456 789 4444") should return true.
+telephoneCheck("123**&!!asdf#") should return false.
+telephoneCheck("55555555") should return false.
+telephoneCheck("(6054756961)") should return false
+telephoneCheck("2 (757) 622-7382") should return false.
+telephoneCheck("0 (757) 622-7382") should return false.
+telephoneCheck("-1 (757) 622-7382") should return false
+telephoneCheck("2 757 622-7382") should return false.
+telephoneCheck("10 (757) 622-7382") should return false.
+telephoneCheck("27576227382") should return false.
+telephoneCheck("(275)76227382") should return false.
+telephoneCheck("2(757)6227382") should return false.
+telephoneCheck("2(757)622-7382") should return false.
+telephoneCheck("555)-555-5555") should return false.
+telephoneCheck("(555-555-5555") should return false.
+telephoneCheck("(555)5(55?)-5555") should return false
 */
+
 //Cash Register
 /*
 
